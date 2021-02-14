48b368099def8679ba705019b241d9db4a9fb276
Marcell Lipp <lipp91.ml@gmail.com>
2021-02-14 21:52:25 +0100

    Fix review findings

--- return42.py
+++ return42.py
@@ -7,5 +7,7 @@ RET_VAL = 43
 #some changes added to the script
 #some comments added
 #the code was ugly, so it has been refactored
+#There was a lot of review finding, they have been fixed
+
 
 sys.exit(RET_VAL)
